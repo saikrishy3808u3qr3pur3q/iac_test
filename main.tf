63348dcd8b00c2a495e704a4e7aa2ecc9cc9d4be
Saikrishnan R <116053032+saikrishy3808u3qr3pur3q@users.noreply.github.com>
2025-12-18 09:50:20 +0530

    Add S3 bucket and public access block configuration
    Added S3 bucket and public access block resources.

--- main.tf
+++ main.tf
@@ -47,3 +47,15 @@ resource "aws_instance" "this" {
    Name = each.key
  }
 }
+resource "aws_s3_bucket" "example" {
+  bucket = "my-private-bucket-123456"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
